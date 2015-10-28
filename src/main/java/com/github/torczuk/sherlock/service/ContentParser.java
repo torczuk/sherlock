f3fdf45187903c75d5693e5f74dc862aef30d60c
arek <torczuk@gmail.com>
2015-10-28 23:28:25 +0100

    Added ContentParser for extracting plain text from html

---
+++ src/main/java/com/github/torczuk/sherlock/service/ContentParser.java
@@ -0,0 +1,28 @@
+package com.github.torczuk.sherlock.service;
+
+import com.github.torczuk.sherlock.domain.Content;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.SAXException;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class ContentParser {
+
+    public String parse(Content content) {
+        try(InputStream stream = new ByteArrayInputStream(content.toString().getBytes(StandardCharsets.UTF_8));) {
+            BodyContentHandler handler = new BodyContentHandler();
+            AutoDetectParser parser = new AutoDetectParser();
+            parser.parse(stream, handler, new Metadata());
+            return handler.toString().trim();
+        } catch (TikaException | SAXException | IOException ex) {
+            throw new RuntimeException("Cannot extract content from page");
+        }
+    }
+
+}
---
+++ src/test/groovy/com/github/torczuk/sherlock/service/ContentParserIntegrationTest.groovy
@@ -0,0 +1,29 @@
+package com.github.torczuk.sherlock.service
+
+import com.github.torczuk.sherlock.domain.Content
+import spock.lang.Specification
+import spock.lang.Subject
+
+class ContentParserIntegrationTest extends Specification {
+
+    private static final String HTML = '''<html>
+                <head></head>
+                <body>
+                    <div id="any">Some text goes here...</div>
+                </body>
+            <html>'''
+
+    @Subject ContentParser parser
+
+    def 'should extract plain text from html page'() {
+        given:
+        Content content = new Content(HTML)
+        parser = new ContentParser()
+
+        when:
+        String text = parser.parse(content)
+
+        then:
+        'Some text goes here' == text
+    }
+}
